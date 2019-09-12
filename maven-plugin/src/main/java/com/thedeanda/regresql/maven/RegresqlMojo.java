90cb6023a2282accddbc15e6de338a9f31261134
Miguel De Anda <miguel@thedeanda.com>
2019-09-11 19:29:23 -0700

    maven plugin tests

--- maven-plugin/src/main/java/com/thedeanda/regresql/maven/RegresqlMojo.java
+++ maven-plugin/src/main/java/com/thedeanda/regresql/maven/RegresqlMojo.java
@@ -3,15 +3,19 @@ package com.thedeanda.regresql.maven;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 
 
 // https://dzone.com/articles/a-simple-maven-3-plugin
-@Mojo(name = "hello")
+@Mojo(name = "hello", defaultPhase = LifecyclePhase.COMPILE)
 public class RegresqlMojo extends AbstractMojo {
+
+    //@Parameter(property = "msg",defaultValue = "from maven")
+    private String msg = "test";
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
+        getLog().info("Hello " + msg);
     }
 }
